3c1e52ddc00c7565e0ac709c02c881938f0ec201
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2024-05-14 23:06:45 +0200

    getter & setter for Garden

--- app/model/src/main/java/com/ftn/sbnz/model/Garden.java
+++ app/model/src/main/java/com/ftn/sbnz/model/Garden.java
@@ -1,10 +1,54 @@
 package com.ftn.sbnz.model;
 
 public class Garden {
+
+    private final Long id;
     private double sizeInSquareMeters;
     private SoilType soilType;
     private int directSunlightExposureInHours;
     private Climate climate;
 
+    public Garden(Long id, double sizeInSquareMeters, SoilType soilType, int directSunlightExposureInHours, Climate climate) {
+        this.id = id;
+        this.sizeInSquareMeters = sizeInSquareMeters;
+        this.soilType = soilType;
+        this.directSunlightExposureInHours = directSunlightExposureInHours;
+        this.climate = climate;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public double getSizeInSquareMeters() {
+        return sizeInSquareMeters;
+    }
+
+    public void setSizeInSquareMeters(double sizeInSquareMeters) {
+        this.sizeInSquareMeters = sizeInSquareMeters;
+    }
+
+    public SoilType getSoilType() {
+        return soilType;
+    }
+
+    public void setSoilType(SoilType soilType) {
+        this.soilType = soilType;
+    }
+
+    public int getDirectSunlightExposureInHours() {
+        return directSunlightExposureInHours;
+    }
+
+    public void setDirectSunlightExposureInHours(int directSunlightExposureInHours) {
+        this.directSunlightExposureInHours = directSunlightExposureInHours;
+    }
+
+    public Climate getClimate() {
+        return climate;
+    }
+
+    public void setClimate(Climate climate) {
+        this.climate = climate;
+    }
 }
